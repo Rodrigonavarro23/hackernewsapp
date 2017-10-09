a30e71b3af41c6d3bba3204c5c7b736149cc97e0
Rodrigo Navarro <rnavarro@acid.cl>
2017-10-09 04:49:02 -0300

    added config to heroku

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ module.exports = {
   isDevelopment: process.env.NODE_ENV === 'development',
   port: process.env.PORT || 3000,
   db: {
+    uri: process.env.MONGODB_URI || null,
     host: process.env.MONGO_HOST || null,
     user: process.env.MONGO_USER || null,
     pass: process.env.MONGO_PASS || null,
